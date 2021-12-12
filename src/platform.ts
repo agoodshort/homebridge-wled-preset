b83c9eac6225957a40a18ebaabed1cea9c49ec7d
Adrien Biencourt <adrien.goodshort@gmail.com>
2021-12-12 20:00:45 +0000

    Created methods, implemented node-fetch to handle unresponsive devices

--- src/platform.ts
+++ src/platform.ts
@@ -11,6 +11,9 @@ import {
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
 import { WledPresetAccessory } from './platformAccessory';
 
+import fetch from 'node-fetch';
+
+
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
@@ -31,11 +34,6 @@ export class WledPresetPlatform implements DynamicPlatformPlugin {
 
     if (!this.config.wleds) {
       this.log.info('No WLEDs have been configured');
-    } else {
-      const wledsRecord = this.config.wleds as Record<string, Array<string>>;
-      for (const k in wledsRecord) {
-        this.log.info(wledsRecord[k]['name']);
-      }
     }
 
     this.log.debug('Finished initializing platform:', this.config.name);
@@ -46,10 +44,13 @@ export class WledPresetPlatform implements DynamicPlatformPlugin {
     // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
-      // run the method to discover / register your devices as accessories
-      this.retrieveDevicesFromConfig();
+      const wledDevices = this.retrieveDevicesFromConfig();
+      this.registerDevices(wledDevices);
     });
   }
+
+  /* ------------------------------------------------------------------------------------------------------------------------------- */
+  /* METHODS */
 
   /**
    * This function is invoked when homebridge restores cached accessories from disk at startup.
@@ -57,94 +58,107 @@ export class WledPresetPlatform implements DynamicPlatformPlugin {
    */
   configureAccessory(accessory: PlatformAccessory) {
     this.log.info('Loading accessory from cache:', accessory.displayName);
-
     // add the restored accessory to the accessories cache so we can track if it has already been registered
-    this.accessories.push(accessory);
+    this.accessories.push(accessory); 
   }
 
+  /**
+   * Retrieve devices from the config.json file
+   */
   retrieveDevicesFromConfig() {
     const wledsRecord = this.config.wleds as Record<string, Array<string>>;
     const wledDevices: { displayName: string; ip: string; presetsNb: number }[] = [];
-    
+        
     for (const k in wledsRecord) {
+      this.log.info('Retrieving settings from config file:', wledsRecord[k]['name']);
       wledDevices.push({
         displayName: wledsRecord[k]['name'] as string,
         ip: wledsRecord[k]['ip'] as string,
         presetsNb: wledsRecord[k]['presetsNb'] as number,
       });
     }
+    return wledDevices;
+  }
 
-    // loop over the discovered devices and register each one if it has not already been registered
+  /**
+   * Register an array of WLED devices as accessories in Homebridge
+   * 
+   * @param wledDevices array of WLED devices
+   */
+  registerDevices(wledDevices: { displayName: string; ip: string; presetsNb: number }[]) {
+
+    // loop over the retrieved devices and register each one if it has not already been registered
     for (const device of wledDevices) {
-      // generate a unique id for the accessory this should be generated from
-      // something globally unique, but constant, for example, the device serial
-      // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.ip);
+      this.log.debug('Making sure ' + device.displayName + ' is reachable...');
+      fetch('http://' + device.ip + '/win')
+        .then(response => {
+          if (!response.ok) {
+            this.log.error('Request to ' + device.displayName + ' failed with status ${response.status}');
+          } else {
+            this.log.info(device.displayName + ' is reachable');
 
-      // see if an accessory with the same uuid has already been registered and restored from
-      // the cached devices we stored in the `configureAccessory` method above
-      const existingAccessory = this.accessories.find(
-        (accessory) => accessory.UUID === uuid,
-      );
+            // generate a unique id for the accessory from IP address
+            const uuid = this.api.hap.uuid.generate(device.ip);
 
-      if (existingAccessory) {
-        // the accessory already exists
-        if (device) {
-          this.log.info(
-            'Restoring existing accessory from cache:',
-            existingAccessory.displayName,
-          );
+            // see if an accessory with the same uuid has already been registered and restored from
+            // the cached devices we stored in the `configureAccessory` method above
+            const existingAccessory = this.accessories.find((accessory) => accessory.UUID === uuid);
 
-          // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-          existingAccessory.context.device = device;
-          this.api.updatePlatformAccessories([existingAccessory]); // To-Do: What does this do
+            if (existingAccessory) {
+            // the accessory already exists
+              this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
 
-          // create the accessory handler for the restored accessory
-          // this is imported from `platformAccessory.ts`
-          new WledPresetAccessory(
-            this,
-            existingAccessory,
-            device.displayName,
-            device.ip,
-            device.presetsNb,
-          );
+              // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
+              existingAccessory.context.device = device;
+              this.api.updatePlatformAccessories([existingAccessory]);
 
-          // update accessory cache with any changes to the accessory details and information
-          this.api.updatePlatformAccessories([existingAccessory]);
-        } else if (!device) {
-          // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-          // remove platform accessories when no longer present
-          this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
-            existingAccessory,
-          ]);
-          this.log.info(
-            'Removing existing accessory from cache:',
-            existingAccessory.displayName,
-          );
-        }
-      } else {
-        // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.displayName);
+              // create the accessory handler for the restored accessory
+              // this is imported from `platformAccessory.ts`
+              new WledPresetAccessory(
+                this,
+                existingAccessory,
+                device.displayName,
+                device.ip,
+                device.presetsNb,
+              );
 
-        // create a new accessory
-        const accessory = new this.api.platformAccessory(
-          device.displayName,
-          uuid,
-        );
+              // update accessory cache with any changes to the accessory details and information
+              this.api.updatePlatformAccessories([existingAccessory]);
 
-        // store a copy of the device object in the `accessory.context`
-        // the `context` property can be used to store any data about the accessory you may need
-        accessory.context.device = device;
+            } else {
+            // the accessory does not yet exist, so we need to create it
+              this.log.info('Adding new accessory:', device.displayName);
 
-        // create the accessory handler for the newly create accessory
-        // this is imported from `platformAccessory.ts`
-        new WledPresetAccessory(this, accessory, device.displayName, device.ip, device.presetsNb);
+              // create a new accessory
+              const accessory = new this.api.platformAccessory(
+                device.displayName,
+                uuid,
+              );
 
-        // link the accessory to your platform
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [
-          accessory,
-        ]);
-      }
+              // store a copy of the device object in the `accessory.context`
+              // the `context` property can be used to store any data about the accessory you may need
+              accessory.context.device = device;
+
+              // create the accessory handler for the newly create accessory
+              // this is imported from `platformAccessory.ts`
+              new WledPresetAccessory(this, accessory, device.displayName, device.ip, device.presetsNb);
+
+              // link the accessory to your platform
+              this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+            }
+          }
+        })
+        .catch(error => {
+          this.log.error(device.displayName + ': ' + error);
+          const uuid = this.api.hap.uuid.generate(device.ip);
+          const existingAccessory = this.accessories.find((accessory) => accessory.UUID === uuid);
+
+          // remove platform accessory when no longer present
+          if (existingAccessory) {
+            this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+            this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+          }
+        });
     }
   }
 }
